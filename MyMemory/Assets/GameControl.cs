a87ede610296c8b5bcb4898ffa10d289a9694d85
Abdulkadir Gür <58683068+AbdulkadirGur@users.noreply.github.com>
2022-12-19 18:42:46 +0300

    Add new assets

--- MyMemory/Assets/GameControl.cs
+++ MyMemory/Assets/GameControl.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
     int numChoices;
+
+    GameObject chooseButton;
+    GameObject itselfBTN;
+
+    public Sprite defaultSprite;
     void Start()
     {
         numChoices = 0;
@@ -16,23 +22,43 @@ public class GameControl : MonoBehaviour
         
     }
 
+    public void GetObjects(GameObject obj)
+    {
+        itselfBTN=obj;
+        itselfBTN.GetComponent<Image>().sprite = itselfBTN.GetComponentInChildren<SpriteRenderer>().sprite;
+    }
     public void MyButtonClick(int value)
     {
-        if(numChoices == 0)// first button click 
+        Control(value,itselfBTN);
+    }
+
+    void Control(int getValue ,GameObject getObje)
+    {
+        if (numChoices == 0)// first button click 
         {
-            numChoices = value;
+            numChoices = getValue;
+            chooseButton= getObje;
         }
         else // Pressing any button a second time
         {
-            if(numChoices == value)
+            if (numChoices == getValue)
             {
-                Debug.Log("Eslesti");
+                //Debug.Log("Matched");
+                Destroy(chooseButton.gameObject);
+                Destroy(getObje.gameObject);
+
                 numChoices = 0;
+                chooseButton = null;
+
             }
             else
             {
-                Debug.Log("Eslesmedi");
+                // Debug.Log("did not Matched");
+                chooseButton.GetComponent<Image>().sprite = defaultSprite;
+                getObje.GetComponent<Image>().sprite = defaultSprite;
+              
                 numChoices = 0;
+                chooseButton = null;
             }
         }
     }
